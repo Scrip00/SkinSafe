40da00b7bd09327dcde9b035d9ad3aa10fe25395
Scrip0 <lavrenenko.2003@gmail.com>
2022-04-10 18:56:29 -0400

    Added some style to UI

--- app/src/main/java/com/skinsafe/skinsafe/MainMenu/MainActivity.java
+++ app/src/main/java/com/skinsafe/skinsafe/MainMenu/MainActivity.java
@@ -1,16 +1,7 @@
 package com.skinsafe.skinsafe.MainMenu;
-import android.annotation.SuppressLint;
-import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import android.content.res.AssetFileDescriptor;
-import android.graphics.Bitmap;
-import android.graphics.Color;
-import android.graphics.Matrix;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,32 +10,23 @@ import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.skinsafe.skinsafe.R;
-import com.skinsafe.skinsafe.ResultActivity;
-
-import org.tensorflow.lite.DataType;
-import org.tensorflow.lite.Interpreter;
-import org.tensorflow.lite.support.image.TensorImage;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.nio.MappedByteBuffer;
-import java.nio.channels.FileChannel;
 
 public class MainActivity extends AppCompatActivity {
 
-    private BottomNavigationView bottomNav;
+    private BottomNavigationView upperNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
-        bottomNav = findViewById(R.id.upper_navigation);
-        bottomNav.setOnItemSelectedListener(navListener);
+
+        upperNav = findViewById(R.id.upper_navigation);
+        upperNav.setOnItemSelectedListener(navListener);
         Intent intent = getIntent();
 
         if (intent.getStringExtra("frag") != null && intent.getStringExtra("frag").equals("track")){
-            bottomNav.setSelectedItemId(R.id.nav_track);
+            upperNav.setSelectedItemId(R.id.nav_track);
         }
 
         if (getIntent().getStringExtra("selectedFragment") != null && getIntent().getStringExtra("selectedFragment").equals("track")) {
